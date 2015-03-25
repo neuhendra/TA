93951a416aea173716c8d962682c4eeea21346d2
neuhendra <neuhendra@gmail.com>
2015-03-25 23:04:57 +0700

    pertama stage

---
+++ 11Maret/11Maret/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace _11Maret
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.videoControl1 = new CCT.NUI.Visual.VideoControl();
+            this.buttonHandAndFinger = new System.Windows.Forms.Button();
+            this.radioButtonKinectWONear = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // videoControl1
+            // 
+            this.videoControl1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.videoControl1.BackColor = System.Drawing.Color.Black;
+            this.videoControl1.Location = new System.Drawing.Point(133, 12);
+            this.videoControl1.Name = "videoControl1";
+            this.videoControl1.Size = new System.Drawing.Size(640, 652);
+            this.videoControl1.Stretch = false;
+            this.videoControl1.TabIndex = 8;
+            // 
+            // buttonHandAndFinger
+            // 
+            this.buttonHandAndFinger.Location = new System.Drawing.Point(34, 31);
+            this.buttonHandAndFinger.Name = "buttonHandAndFinger";
+            this.buttonHandAndFinger.Size = new System.Drawing.Size(75, 23);
+            this.buttonHandAndFinger.TabIndex = 7;
+            this.buttonHandAndFinger.Text = "detection";
+            this.buttonHandAndFinger.UseVisualStyleBackColor = true;
+            this.buttonHandAndFinger.Click += new System.EventHandler(this.buttonHandAndFinger_Click);
+            // 
+            // radioButtonKinectWONear
+            // 
+            this.radioButtonKinectWONear.AutoSize = true;
+            this.radioButtonKinectWONear.Location = new System.Drawing.Point(24, 74);
+            this.radioButtonKinectWONear.Name = "radioButtonKinectWONear";
+            this.radioButtonKinectWONear.Size = new System.Drawing.Size(81, 17);
+            this.radioButtonKinectWONear.TabIndex = 14;
+            this.radioButtonKinectWONear.Text = "KInect SDK";
+            this.radioButtonKinectWONear.UseVisualStyleBackColor = true;
+            this.radioButtonKinectWONear.CheckedChanged += new System.EventHandler(this.radioButtonKinectWONear_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(785, 715);
+            this.Controls.Add(this.radioButtonKinectWONear);
+            this.Controls.Add(this.buttonHandAndFinger);
+            this.Controls.Add(this.videoControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private CCT.NUI.Visual.VideoControl videoControl1;
+        private System.Windows.Forms.Button buttonHandAndFinger;
+        private System.Windows.Forms.RadioButton radioButtonKinectWONear;
+    }
+}
+
---
+++ 11Maret/11Maret/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+using CCT.NUI.Visual;
+using CCT.NUI.Core;
+using CCT.NUI.Core.Clustering;
+using CCT.NUI.HandTracking;
+using CCT.NUI.Core.OpenNI;
+using CCT.NUI.Core.Shape;
+using CCT.NUI.KinectSDK;
+using CCT.NUI.Core.Video;
+
+namespace _11Maret
+{
+    public partial class Form1 : Form
+    {
+        private IList<IDataSource> activeDataSources;
+        private IDataSourceFactory dataSourceFactory;
+
+        private ClusterDataSourceSettings clusteringSetting = new ClusterDataSourceSettings();
+        private ShapeDataSourceSettings shapeSettings = new ShapeDataSourceSettings();
+        private HandDataSourceSettings handDetectionSettings = new HandDataSourceSettings();
+        public Form1()
+        {
+            InitializeComponent();
+            this.activeDataSources = new List<IDataSource>();
+            this.FormClosing += new FormClosingEventHandler(Form1_FormClosing);
+        }
+
+        void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            this.Clear();
+            if (this.dataSourceFactory != null)
+            {
+                this.dataSourceFactory.Dispose();
+            }
+        }
+
+        private void SetDataSource(IDataSource dataSource, ILayer layer)
+        {
+            this.Clear();
+            this.activeDataSources.Add(dataSource);
+            this.videoControl1.AddLayer(layer);
+            dataSource.Start();
+        }
+
+        private void SetHandDataSource(IHandDataSource dataSource)
+        {
+            this.SetDataSource(dataSource, new HandLayer(dataSource));
+        }
+
+        private void buttonHandAndFinger_Click(object sender, EventArgs e)
+        {
+            this.SetHandDataSource(new HandDataSource(this.dataSourceFactory.CreateShapeDataSource(this.clusteringSetting, this.shapeSettings), this.handDetectionSettings));
+        }
+
+        private void Clear()
+        {
+            foreach (var dataSource in this.activeDataSources)
+            {
+                dataSource.Stop();
+            }
+            this.activeDataSources.Clear();
+            this.videoControl1.Clear();
+        }
+
+        private void radioButtonKinectWONear_CheckedChanged(object sender, EventArgs e)
+        {
+            Cursor.Current = Cursors.WaitCursor;
+            try
+            {
+                this.clusteringSetting.MaximumDepthThreshold = 1000;
+                this.dataSourceFactory = new SDKDataSourceFactory(useNearMode: false);
+            }
+            catch (Exception exc)
+            {
+                Cursor.Current = Cursors.Default;
+                MessageBox.Show(exc.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            Cursor.Current = Cursors.Default;
+        }
+        
+        /*private void ToggleButtons()
+        {
+            this.Enable(this.bu)
+        }*/
+
+        private void SetEnabled(IEnumerable<Control> controls, bool value)
+        {
+            foreach (var control in controls)
+            {
+                control.Enabled = value;
+            }
+        }
+
+        private void Enable(params Control[] controls)
+        {
+            this.SetEnabled(controls, true);
+        }
+
+        private void Disable(params Control[] controls)
+        {
+            this.SetEnabled(controls, false);
+        }
+    }
+}
